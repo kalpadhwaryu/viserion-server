bc23fee1f402b1396f6a276f1a98e738adb730df
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2023-08-08 22:22:30 +0530

    Added types and error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 
-const importDynamic = new Function("modulePath", "return import(modulePath)");
+const importDynamic = new Function(
+  "modulePath",
+  "return import(modulePath)"
+) as (modulePath: string) => Promise<any>;
 
-const fetch = async (...args: any[]) => {
+const fetch = async (...args: any[]): Promise<any> => {
   const module = await importDynamic("node-fetch");
   return module.default(...args);
 };
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -18,7 +21,7 @@ app.use(cors());
 dotenv.config();
 
 // Gets GitHub Access Token
-app.get("github/getAccessToken", async (req: Request, res: Response) => {
+app.get("/github/getAccessToken", async (req: Request, res: Response) => {
   const params =
     "?client_id=" +
     process.env.GITHUB_CLIENT_ID +
@@ -26,25 +29,26 @@ app.get("github/getAccessToken", async (req: Request, res: Response) => {
     process.env.GITHUB_CLIENT_SECRET +
     "&code=" +
     req.query.code;
-  await fetch("https://github.com/login/oauth/access_token" + params, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      res.json(data);
-    });
+  try {
+    const response = await fetch(
+      "https://github.com/login/oauth/access_token" + params,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 // Gets Users Followers and Repos
-// Access Token is going to be passed in as an Authorization header
 app.get("/github/:entity", async (req: Request, res: Response) => {
-  const entity = req.params.entity;
-
+  const entity: string = req.params.entity;
   try {
     const userResponse = await fetch("https://api.github.com/user", {
       method: "GET",
@@ -54,9 +58,7 @@ app.get("/github/:entity", async (req: Request, res: Response) => {
     });
 
     const userData = await userResponse.json();
-
-    let dataUrl;
-
+    let dataUrl: string;
     if (entity === "followers") {
       dataUrl = userData.followers_url;
     } else if (entity === "repos") {
@@ -71,10 +73,9 @@ app.get("/github/:entity", async (req: Request, res: Response) => {
         Authorization: req.header("Authorization"),
       },
     });
-
     const responseData = await dataResponse.json();
-
     res.json(responseData);
+
   } catch (error) {
     res.status(500).json({ error: "An error occurred" });
   }
@@ -89,50 +90,56 @@ app.post("/jira/getAccessToken", async (req: Request, res: Response) => {
     code: req.body.code,
     redirect_uri: "https://localhost:3000/",
   };
-  await fetch("https://auth.atlassian.com/oauth/token", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestBody),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      res.json(data);
+  try {
+    const response = await fetch("https://auth.atlassian.com/oauth/token", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
     });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 // Get Jira Projects/Dashboards
 app.get("/jira/:entity", async (req: Request, res: Response) => {
   const entity = req.params.entity;
-  const availableResourceResponse = await fetch(
-    "https://api.atlassian.com/oauth/token/accessible-resources",
-    {
-      method: "GET",
-      headers: {
-        Authorization: req.header("Authorization"),
-      },
-    }
-  );
-  const availableResourceData = await availableResourceResponse.json();
-  const cloudId = availableResourceData[0].id;
 
-  const dataResponse = await fetch(
-    `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/2/${entity}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: req.header("Authorization"),
-      },
-    }
-  );
+  try {
+    const availableResourceResponse = await fetch(
+      "https://api.atlassian.com/oauth/token/accessible-resources",
+      {
+        method: "GET",
+        headers: {
+          Authorization: req.header("Authorization"),
+        },
+      }
+    );
 
-  const responseData = await dataResponse.json();
+    const availableResourceData = await availableResourceResponse.json();
+    const cloudId = availableResourceData[0].id;
 
-  res.json(responseData);
+    const dataResponse = await fetch(
+      `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/2/${entity}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: req.header("Authorization"),
+        },
+      }
+    );
+
+    const responseData = await dataResponse.json();
+    res.json(responseData);
+    
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
